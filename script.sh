a4d0f1f9d49be7260f9f3324725c983d2931ab2a
KvukRX <k.v.u.k@yandex.ru>
2024-06-30 16:33:24 +0300

    final version

--- script.sh
+++ script.sh
@@ -1,1 +1,18 @@
 #!/bin/bash
+
+
+sudo apt -y install cmake ninja-build meson libinput-dev
+
+cd ~
+
+git clone https://gitlab.com/warningnonpotablewater/libinput-config
+
+cd libinpit-config
+meson build
+cd build
+ninja
+sudo ninja install
+
+sudo echo "scroll-factor=$arg" >> /etc/libinput.conf
+
+sudo reboot
